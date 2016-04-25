d484c0fba607a5a80b013518f6aad40bfce10702
Kelsey <kelseyoo14@gmail.com>
2016-04-24 18:07:20 -0700

    finished

---
+++ application.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index_page():
+    """Show an index page."""
+
+    return render_template("index.html")
+
+    # Alternately, we could make this a Jinja template in `templates/`
+    # and return that result of rendering this, like:
+    #
+    # return render_template("index.html")
+
+
+@app.route("/application_form")
+def application():
+    """Returns application-form.html"""
+
+    return render_template("application-form.html")
+
+
+@app.route("/application", methods=["POST"])
+def return_app_info():
+
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    salary = request.form.get("salaryrequirement")
+    job_title = request.form.get("job")
+    fullname = first_name + " " + last_name
+
+    return render_template("application-response.html",
+                            full_name=fullname,
+                            salary_amount=salary,
+                            job=job_title)
+
+if __name__ == "__main__":
+    app.run(debug=True)
